17b38da50a934105beef2f091a1c382496aa8f4f
Victor Inácio <chustrupgamer@gmail.com>
2023-08-16 16:06:17 -0300

    Initial Commit

---
+++ login-with-github/login_with_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_with_githubApp.swift
+//  login-with-github
+//
+//  Created by Victor Soares on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct login_with_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
